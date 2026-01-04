f4b3fca9becd71c58b42f327dadb0eee8307cdcf
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2026-01-04 22:46:07 +0600

    collapse complete

--- src/Components/WhatBest/WhatBest.tsx
+++ src/Components/WhatBest/WhatBest.tsx
@@ -1,23 +1,12 @@
 import { ChevronDown } from "lucide-react";
-import { useRef, useState } from "react";
+import {  useState, type ElementType } from "react";
 import { WhyBest1, WhyBest2, WhyBest3 } from "../../Icons";
 import Container from "../../Shared/Container";
 
 export default function WhatBest() {
-  const [openCollapse, setOpenCollapse] = useState<boolean>(false);
-  const collapseRef = useRef<HTMLDivElement | null>(null);
-  const handleClick = () => {
-    setOpenCollapse((prev) => !prev);
-
-    if (!openCollapse) {
-      return null;
-    }
-  };
-
-  console.log(openCollapse);
-
   type BestArrType = {
     // icon: ReactNode;
+    icon :ElementType
     title: string;
     description: string;
   };
@@ -58,18 +47,16 @@ export default function WhatBest() {
 
           <div className="space-y-5 my-10">
             {bestArr.map((best, index) => (
-              <div
-                key={index}
-                onClick={() => setSelected(index)}
-                className="border relative  rounded-xl space-y-4"
-              >
+              <div key={index} className="border relative  rounded-xl  ">
                 <div
-                  onClick={handleClick}
-                  className="flex justify-between cursor-pointer px-6 pt-6"
+                  onClick={() => setSelected(index)}
+                  className={`flex justify-between cursor-pointer  ${
+                    index === selected ? "pt-6 px-6" : "p-6"
+                  }`}
                 >
                   <div className="flex gap-5  ">
                     <best.icon />
-                    {index === selected ? 1 : 0}
+
                     <div>
                       <h3 className="text-2xl font-semibold">{best?.title}</h3>
                     </div>
@@ -79,9 +66,15 @@ export default function WhatBest() {
                   </div>
                 </div>
 
-                <div ref={collapseRef} className="flex ">
-                  <div className="w-19  shrink-0"></div>
-                  <p className="text-lg text-[#212529]  pb-6 max-w-[452px] ">
+                <div
+                  className={`flex transition-all duration-1000 ease-in-out ${
+                    index === selected
+                      ? "max-h-50 opacity-100 pt-3 pb-6 visible"
+                      : "max-h-0 opacity-0 invisible "
+                  }`}
+                >
+                  <div className="w-19 shrink-0"></div>
+                  <p className="text-lg text-[#212529] max-w-113">
                     {best?.description}
                   </p>
                 </div>
