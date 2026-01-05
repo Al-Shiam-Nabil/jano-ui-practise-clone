8ea4e5e2234b6a10fcde66772b82a025739d12dd
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2026-01-05 11:57:47 +0600

    what best without respoinsive

--- src/Components/WhatBest/WhatBest.tsx
+++ src/Components/WhatBest/WhatBest.tsx
@@ -1,7 +1,10 @@
 import { ChevronDown } from "lucide-react";
 import { useState, type ElementType } from "react";
-import { WhyBest1, WhyBest2, WhyBest3 } from "../../Icons";
+import { ThreeLine, WhyBest1, WhyBest2, WhyBest3 } from "../../Icons";
 import Container from "../../Shared/Container";
+
+import RectangleDot from "../../Icons/rectangleDot.svg";
+import SmallDot from "../../Icons/smallSqureDot.svg";
 
 export default function WhatBest() {
   const [selected, setSelected] = useState<number | null>(null);
@@ -33,13 +36,57 @@ export default function WhatBest() {
   ];
 
   return (
-    <Container>
-      <div className="flex">
+    <Container className="my-35">
+      <div className="flex gap-40">
         {/* left */}
-        <div className="w-1/2"></div>
+        <div className="w-full lg:w-1/2 space-y-12 ">
+          <div className="flex justify-between items-end gap-12">
+            <div>
+              <img src={SmallDot} alt="image" />
+            </div>
+
+            <div className="border border-[#ffae10] space-y-4 p-8.5 max-w-[302px] bg-[#fff3c9] rounded-tl-[40px] rounded-br-[40px]">
+              <h3 className="text-4xl font-semibold text-[#ffae10]">
+                10million+
+              </h3>
+              <p className="text-lg text-black/70">
+                Customers trust us & have bought insurance.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex justify-between gap-12">
+            <div className="border border-[#9650ef] bg-[#a25bfb33] max-w-[292px] space-y-4 p-8.5 rounded-tl-[40px] rounded-br-[40px] flex flex-col justify-center relative">
+              <h3 className="text-[#9650ef] text-4xl font-semibold">
+                50+ Insurance
+              </h3>
+              <p className="text-lg text-black/70">
+                Partnered with us so that you can compare easily & transparently
+              </p>
+
+              <div className="absolute -bottom-12 -left-12">
+                <ThreeLine />
+              </div>
+            </div>
+            <div>
+              <div className="border border-[#00bde6] space-y-4 p-8.5 bg-[#00d1ff33] rounded-tl-[40px] rounded-br-[40px]">
+                <h3 className="text-[27px] text-[#00bde6] font-semibold">
+                  Claims
+                </h3>
+                <p className="text-lg text-black/70">
+                  Get Support by expert easily.
+                </p>
+              </div>
+
+              <div className="mt-7.5">
+                <img src={RectangleDot} alt="image" />
+              </div>
+            </div>
+          </div>
+        </div>
 
         {/* right */}
-        <div className="w-1/2">
+        <div className="w-full lg:w-1/2">
           <p className="text-black/30 uppercase">Why Choose Us</p>
           <h3 className="text-[58px] font-semibold">What makes us the best.</h3>
 
--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -5,6 +5,9 @@ import Discover4 from "../Icons/discover4.svg?react";
 import Icon1 from "../Icons/icon_1.svg?react";
 import Icon2 from "../Icons/icon_2.svg?react";
 import Icon3 from "../Icons/icon_3.svg?react";
+import RectangleDot from "../Icons/rectangleDot.svg?react";
+import SmallSqureDot from "../Icons/smallSqureDot.svg?react";
+import ThreeLine from "../Icons/threeLine.svg?react";
 import WhyBest1 from "../Icons/whyBest1.svg?react";
 import WhyBest2 from "../Icons/whyBest2.svg?react";
 import WhyBest3 from "../Icons/whyBest3.svg?react";
@@ -17,6 +20,9 @@ export {
   Icon1,
   Icon2,
   Icon3,
+  RectangleDot,
+  SmallSqureDot,
+  ThreeLine,
   WhyBest1,
   WhyBest2,
   WhyBest3,
